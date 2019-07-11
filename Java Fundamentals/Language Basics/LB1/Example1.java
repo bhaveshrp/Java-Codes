e853d94e81b3a75a7aa94aa5a48315649f4fbdab
Bhavesh Rajendra Patil <42971324+bhaveshrp@users.noreply.github.com>
2019-07-11 10:19:48 +0530

    Create Example1.java

---
+++ Java Fundamentals/Language Basics/LB1/Example1.java
@@ -0,0 +1,10 @@
+//Example1) If the two command line arguments are Wipro and Bangalore then the output generated should be Wipro Technologies Bangalore.
+
+import java.util.*;
+import java.io.*;
+
+class Example1 {
+    public static void main(String args[]){
+        System.out.println(args[0] + " Technologies " + args[1]);
+    }
+}
