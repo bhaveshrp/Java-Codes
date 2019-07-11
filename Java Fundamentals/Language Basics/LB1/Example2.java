861ba74419a8f737ab966537c67a887fd1beda8d
Bhavesh Rajendra Patil <42971324+bhaveshrp@users.noreply.github.com>
2019-07-11 10:20:13 +0530

    Add files via upload

---
+++ Java Fundamentals/Language Basics/LB1/Example2.java
@@ -0,0 +1,10 @@
+//Example2) If the command line arguments are ABC and Mumbai then the output generated should be ABC Technologies Mumbai
+
+import java.util.*;
+import java.io.*;
+
+class Example2 {
+    public static void main(String args[]){
+        System.out.println(args[0] + " Technologies " + args[1]);
+    }
+}
